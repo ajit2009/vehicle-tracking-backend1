292d06e70a6f930054e05e74969434367c4d4d26
ajit2009 <65666054+ajit2009@users.noreply.github.com>
2025-04-02 17:32:27 +0530

    Create db_config.py

---
+++ db_config.py
@@ -0,0 +1,8 @@
+# db_config.py
+
+db_config = {
+    'host': 'sql7.freesqldatabase.com',  # Replace with your free hosting SQL server
+    'user': 'sql7770632',               # Replace with your database username
+    'password': ' rW4FZ1M34e',       # Replace with your database password
+    'database': 'sql7770632'  # Replace with your database name
+}
